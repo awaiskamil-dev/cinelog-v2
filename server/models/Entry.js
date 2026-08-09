891c1c9dd6d136a865214c2b5f4c0fac6d89beca
awaiskamil-dev <kamilawais@gmail.com>
2026-08-09 17:12:08 +0500

    Creating entry model

---
+++ server/models/Entry.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose');
+
+const EntrySchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Types.ObjectId,
+    ref: user,
+    required: true,
+  },
+  tmdbId: {
+    type: Number,
+    required: true,
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+  posterPath: {
+    type: String,
+    required: true,
+  },
+  status: {
+    type: String,
+    enum: ['plan-to-watch', 'watching', 'watched'],
+    default: 'plan-to-watch',
+  },
+  rating: {
+    type: Number,
+    min: 1,
+    max: 10,
+  },
+  review: {
+    type: String,
+    maxLength: 2000,
+  },
+  dateWatched: {
+    type: Date,
+  },
+}, {timestamps: true});
+
+module.exports = mongoose.model('Entry', EntrySchema);
