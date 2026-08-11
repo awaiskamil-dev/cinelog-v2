d06ee07a0015aafae4434d64507d8c5a20c9c18a
awaiskamil-dev <kamilawais@gmail.com>
2026-08-11 19:00:12 +0500

    Making search bar interactive

--- client/src/components/SearchBar.jsx
+++ client/src/components/SearchBar.jsx
@@ -1,18 +1,45 @@
 import './SearchBar.css';
+import { useState } from 'react';
 
 function SearchBar(){
+  const [searchInput, setSearchInput] = useState('');
+  const [genreSelect, setGenreSelect] = useState('Any');
+  const [yearSelect, setYearSelect] = useState('Any');
+  const [formatSelect, setFormatSelect] = useState('Any');
+
+  const currentYear = new Date().getFullYear();
+  const startYear = 1970;
+  const endYear = currentYear + 2;
+
+  const years = [];
+  for(let year = endYear; year >= startYear; year--){
+    years.push(year);
+  }
+
   return (
     <div className="search-bar">
       <div className="search-group">
         <label>Search</label>
         <i className="fa-solid fa-magnifying-glass"></i>
-        <input type="text" className="search-input" />
-        <i className="fa-solid fa-xmark js-clear-search hidden"></i>
+        <input 
+          type="text"
+          className="search-input"
+          value={searchInput}
+          onChange={(event) => setSearchInput(event.target.value)}
+        />
+        {searchInput !== '' && (
+          <i className="fa-solid fa-xmark"
+            onClick={() => setSearchInput('')}
+          />
+        )}
       </div>
 
       <div className="genre-group">
         <label>Genre</label>
-        <select className="genre-select">
+        <select className="genre-select"
+          value={genreSelect}
+          onChange={(event) => setGenreSelect(event.target.value)}
+        >
           <option>Any</option>
           <option>Action</option>
           <option>Romance</option>
@@ -23,28 +50,47 @@ function SearchBar(){
           <option>Animation</option>
           <option>Thriller</option>
         </select>
-        <i className="fa-solid fa-angle-down js-angle-down-genre"></i>
-        <i className="fa-solid fa-xmark js-clear-genre hidden"></i>
+        {genreSelect !== 'Any'?
+          (<i className="fa-solid fa-xmark" onClick={() => setGenreSelect('Any')}/>)
+          :(<i className="fa-solid fa-angle-down"/>)
+        }
       </div>      
       
       <div className="year-group">
         <label>Year</label>
-        <select className="year-select">
+        <select className="year-select"
+          value={yearSelect}
+          onChange={(event) => setYearSelect(event.target.value)}
+        >
           <option>Any</option>
+          {
+            years.map((year) => {
+              return (
+                <option key={year}>{year}</option>
+              );
+            })
+          }
         </select>
-        <i className="fa-solid fa-angle-down js-angle-down-year"></i>
-        <i className="fa-solid fa-xmark js-clear-year hidden"></i>
+        {yearSelect === 'Any' ? 
+          (<i className="fa-solid fa-angle-down"/>) 
+          :(<i className="fa-solid fa-xmark" onClick={() => setYearSelect('Any')}/>)
+        }
       </div>
 
       <div className="format-group">
         <label>Format</label>
-        <select className="format-select">
+        <select className="format-select"
+          value={formatSelect}
+          onChange={(event) => setFormatSelect(event.target.value)}
+        >
           <option>Any</option>
           <option>Movie</option>
           <option>Series</option>
         </select>
-        <i className="fa-solid fa-angle-down js-angle-down-format"></i>
-        <i className="fa-solid fa-xmark js-clear-format hidden"></i>
+        {formatSelect === 'Any' ? 
+          ( <i className="fa-solid fa-angle-down"/>)
+          :(<i className="fa-solid fa-xmark" onClick={() => setFormatSelect('Any')}/>)
+        }
       </div>
     </div>
   );
