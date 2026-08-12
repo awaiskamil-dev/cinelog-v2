e6fabe7bf9f9da25d83d355674866eae7efe75d6
awaiskamil-dev <kamilawais@gmail.com>
2026-08-12 19:13:37 +0500

    Creating movie and tv controllers

--- server/controllers/movieController.js
+++ server/controllers/movieController.js
@@ -1,33 +1,102 @@
+require('dotenv').config();
+const {StatusCodes} = require('http-status-codes');
+
+const movieGenreMap = {
+  action: 28, comedy: 35, drama: 18, horror: 27,
+  'sci-fi': 878, romance: 10749, animation: 16, thriller: 53,
+};
+
+const tvGenreMap = {
+  action: 10759, comedy: 35, drama: 18, horror: 9648,
+  'sci-fi': 10765, romance: 10766, animation: 16, thriller: 9648,
+};
+
 const getTrending = async (req, res) => {
-  res.send('getting trending movies')
+  const response = await fetch(`https://api.themoviedb.org/3/trending/all/week?api_key=${process.env.TMDB_API_KEY}`);
+  const data = await response.json();
+  
+  res.status(StatusCodes.OK).json(data);
 };
 
 const getPopular = async (req, res) => {
-  res.send('getting popular movies')
+  const response = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${process.env.TMDB_API_KEY}`);
+  const data = await response.json();
+  
+  res.status(StatusCodes.OK).json(data);
 };
 
 const getTopRated = async (req, res) => {
-  res.send('getting trending movies')
+  const response = await fetch(`https://api.themoviedb.org/3/movie/top_rated?api_key=${process.env.TMDB_API_KEY}`);
+  const data = await response.json();
+  
+  res.status(StatusCodes.OK).json(data);
 };
 
 const getUpcoming = async (req, res) => {
-  res.send('getting trending movies')
+  const response = await fetch(`https://api.themoviedb.org/3/movie/upcoming?api_key=${process.env.TMDB_API_KEY}`);
+  const data = await response.json();
+  
+  res.status(StatusCodes.OK).json(data);
 };
 
 const getDiscover = async (req, res) => {
-  res.send('getting trending movies')
+  const {genre, year, format, page = 1} = req.query;
+
+  const movieGenreParam = genre ? `&with_genres=${movieGenreMap[genre]}` : '';
+  const tvGenreParam = genre ? `&with_genres=${tvGenreMap[genre]}` : '';
+  const movieYearParam = year ? `&primary_release_year=${year}` : '';
+  const tvYearParam = year ? `&first_air_date_year=${year}` : '';
+
+  const fetches = [];
+
+  if (format !== 'tv') {
+    fetches.push(
+      fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${process.env.TMDB_API_KEY}&sort_by=popularity.desc&page=${page}${movieGenreParam}${movieYearParam}`)
+      .then((response) => response.json())
+      .then((data) => {
+        data.results.forEach((m) => (m.media_type = "movie"));
+        return data.results;
+      })
+    );
+  }
+  if (format !== 'movie') {
+    fetches.push(
+      fetch(`https://api.themoviedb.org/3/discover/tv?api_key=${process.env.TMDB_API_KEY}&sort_by=popularity.desc&page=${page}${tvGenreParam}${tvYearParam}`)
+      .then((response) => response.json())
+      .then((data) => {
+        data.results.forEach((t) => (t.media_type = "tv"));
+        return data.results;
+      })
+    );
+  }
+
+  const resultsArrays = await Promise.all(fetches);
+  const combinedResults = resultsArrays.flat();
+
+  res.status(StatusCodes.OK).json({results: combinedResults});
 };
 
 const getSearch = async (req, res) => {
-  res.send('getting trending movies')
+  const {query} = req.query;
+
+  const response = await fetch(`https://api.themoviedb.org/3/search/multi?&api_key=${process.env.TMDB_API_KEY}&query=${query}`);
+  const data = await response.json();
+  
+  res.status(StatusCodes.OK).json(data);
 };
 
 const getAction = async (req, res) => {
-  res.send('getting trending movies')
+  const response = await fetch(`https://api.themoviedb.org/3/discover/movie?with_genres=28&api_key=${process.env.TMDB_API_KEY}`);
+  const data = await response.json();
+  
+  res.status(StatusCodes.OK).json(data);
 };
 
 const getAnimation = async (req, res) => {
-  res.send('getting trending movies')
+  const response = await fetch(`https://api.themoviedb.org/3/discover/movie?with_genres=16&api_key=${process.env.TMDB_API_KEY}`);
+  const data = await response.json();
+  
+  res.status(StatusCodes.OK).json(data);
 };
 
 module.exports = {
--- server/controllers/tvController.js
+++ server/controllers/tvController.js
@@ -1,9 +1,18 @@
+require('dotenv').config();
+const {StatusCodes} = require('http-status-codes');
+
 const getPopular = async (req, res) => {
-  res.send('getting popular tv');
+  const response = await fetch(`https://api.themoviedb.org/3/tv/popular?api_key=${process.env.TMDB_API_KEY}`);
+  const data = await response.json();
+  
+  res.status(StatusCodes.OK).json(data);
 };
 
 const getTopRated = async (req, res) => {
-  res.send('getting top rated tv');
+  const response = await fetch(`https://api.themoviedb.org/3/tv/top_rated?api_key=${process.env.TMDB_API_KEY}`);
+  const data = await response.json();
+  
+  res.status(StatusCodes.OK).json(data);
 };
 
 module.exports = {
