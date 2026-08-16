24a5620a346784ab00e1a0bc30b93584cb353bf5
awaiskamil-dev <kamilawais@gmail.com>
2026-08-16 22:39:00 +0500

    Displaying different elements based on the state of login and handling logout

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,9 +1,42 @@
 import './Navbar.css';
 import logo from '../assets/cl-logo.png';
-import { Link } from 'react-router';
+import { Link, useNavigate } from 'react-router';
+import useAuth from '../context/useAuth';
+import defaultAvatar from '../assets/default-pfp.png';
+import { useEffect, useRef, useState } from 'react';
+import API_URL from '../config';
 
 function Navbar(){
- return (
+  const {user, setUser} = useAuth();
+  const [isOpen, setIsOpen] = useState(false);
+  const isReady = useRef(false);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    isReady.current = false;
+    if(user){
+      const timer = setTimeout(() => {
+        isReady.current = true;
+      }, 10000);  
+      return () => clearTimeout(timer);
+    }
+  }, [user]);
+
+  const handleLogout = async () => {
+    try{
+      await fetch(`${API_URL}/auth/logout`, {
+        method: 'DELETE',
+        credentials: 'include'
+      });
+      setUser(null);
+      setIsOpen(false);
+      navigate('/');
+    }catch(err){
+      console.log(err);
+    }
+  };
+
+  return (
   <nav>
     <div className="nav-left-section">
       <Link to="/">
@@ -15,12 +48,50 @@ function Navbar(){
       <Link className="home-link" to="/">
         Home
       </Link>
-      <Link to="/watchlist">
-        Watchlist
-      </Link>
+      {user? 
+      ( 
+        <>
+          <Link to="/watchlist" className='watchlist-link'>
+            Watchlist
+          </Link>
+          <div 
+            className="nav-avatar-wrapper"
+            onMouseEnter={() => setIsOpen(true)}
+            onMouseLeave={() => setIsOpen(false)}
+          >
+            <div className="nav-avatar">
+              <img src={defaultAvatar} alt="User avatar" />
+              <i className="fa-solid fa-angle-down"></i>
+            </div>
+
+            {isOpen && (
+              <div className="nav-dropdown">
+                <div className='nav-dropdown-inner'>
+                  <div className='nav-dropdown-item' onClick={handleLogout}>
+                    <i className="fa-solid fa-right-from-bracket"></i>
+                    <span>Logout</span>
+                  </div>
+                </div>
+              </div>
+            )}
+          </div>
+        </>
+      )
+      :(
+        <>
+          <Link to="/login" className='nav-login'>
+            Login
+          </Link>
+          <Link to="/register" className='nav-signup'>
+            Sign Up
+          </Link>
+        </>
+      )}
+     
+      
     </div>
   </nav>
- );
+  );
 };
 
 export default Navbar;
