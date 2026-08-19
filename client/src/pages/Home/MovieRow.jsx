d4a47f28d5577d482593efd5f92b27ce42f1985c
awaiskamil-dev <kamilawais@gmail.com>
2026-08-19 18:18:18 +0500

    Making scroll buttons functional

--- client/src/pages/Home/MovieRow.jsx
+++ client/src/pages/Home/MovieRow.jsx
@@ -1,10 +1,14 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import API_URL from "../../config";
 import './MovieRow.css';
 import MovieCard from "./MovieCard";
 
 const MovieRow = function({title, endpoint, userEntries, onStatusChange}){
   const [results, setResults] = useState([]);
+  const [showLeftBtn, setShowLeftBtn] = useState(false);
+  const [showRightBtn, setShowRightBtn] = useState(true);
+
+  const movieRowRef = useRef(null);
   
   useEffect(() => {
     const fetchResults = async () => {
@@ -16,20 +20,48 @@ const MovieRow = function({title, endpoint, userEntries, onStatusChange}){
     fetchResults();
   }, [endpoint, setResults]);
 
+  useEffect(() => {
+    const cardsDiv = movieRowRef.current;
+    if (!cardsDiv) return;
+
+    const updateButtons = () => {
+      setShowLeftBtn(cardsDiv.scrollLeft > 0);
+      setShowRightBtn(
+        cardsDiv.scrollLeft + cardsDiv.clientWidth < cardsDiv.scrollWidth - 1
+      );
+    };
+
+    updateButtons(); 
+
+    cardsDiv.addEventListener('scroll', updateButtons);
+
+    return () => {
+      cardsDiv.removeEventListener('scroll', updateButtons);
+    };
+  }, [results]);
+
+  const scrollLeft = () => {
+    movieRowRef.current.scrollBy({ left: -600, behavior: 'smooth' });
+  };
+
+  const scrollRight = () => {
+    movieRowRef.current.scrollBy({ left: 600, behavior: 'smooth' });
+  };
+
   return (
     <div className="movie-row-section">
       <h2>{title}</h2>
       <div className="scroll-container">
-        <button className="scroll-left">
+        <button className={`scroll-left ${showLeftBtn? '' : 'hidden'}`} onClick={scrollLeft}>
           <i className="fa-solid fa-angle-left"></i>
         </button>
-        <div className="movie-row-div">
+        <div className="movie-row-div" ref={movieRowRef}>
           {results.map((item) => {
             const myEntry = userEntries.find((entry) => entry.tmdbId === item.id);
             return <MovieCard key={item.id} movie={item} status={myEntry?.status} onStatusChange={onStatusChange}/>
           })}
         </div>
-        <button className="scroll-right">
+        <button className={`scroll-right ${showRightBtn? '' : 'hidden'}`} onClick={scrollRight}>
           <i className="fa-solid fa-angle-right"></i>
         </button>
       </div>
