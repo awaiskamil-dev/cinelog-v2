4398c251581e44a2be401628449cb6443d4e7fa4
awaiskamil-dev <kamilawais@gmail.com>
2026-08-28 18:10:43 +0500

    Making changes in backend for render deployment

--- server/app.js
+++ server/app.js
@@ -36,10 +36,17 @@ const apiLimiter = rateLimiter({
 });
 
 app.set('trust proxy', 1);
+
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://cinelog-tracker.netlify.app"
+];
+
 app.use(cors({
-  origin: 'http://localhost:5173',
-  credentials: true,
+  origin: allowedOrigins,
+  credentials: true
 }));
+
 app.use(helmet());
 app.use(xss());
 app.use(mongoSanitize());
@@ -59,7 +66,7 @@ const port = process.env.PORT || 5000;
 const start = async () => {
   try {
     await connectDB(process.env.MONGO_URL);
-    app.listen(port, () =>
+    app.listen(port, '0.0.0.0', () =>
       console.log(`Server is listening on port ${port}...`)
     );
   } catch (error) {
